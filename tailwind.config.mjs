ce8d84afb714232b38d81b75a3f93eb287ff1ac1
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-02-25 22:07:00 +0100

    Add webmanifest

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,6 @@
+import { defineConfig } from 'astro/config';
+import webmanifest from 'astro-webmanifest';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -5,4 +8,24 @@ export default {
 		extend: {},
 	},
 	plugins: [],
+	integrations: [
+		webmanifest({
+		  /**
+		   * required
+		   **/
+		  name: 'Bergljots',
+	
+		  /**
+		   * optional
+		   **/
+		//   icon: 'src/images/your-icon.svg', // source for favicon & icons
+	
+		  short_name: 'Bergljots',
+		  description: 'På vei ut døra i Bergljots',
+		  start_url: '/',
+		  theme_color: '#3367D6',
+		  background_color: '#3367D6',
+		  display: 'standalone',
+		}),
+	  ],
 }
