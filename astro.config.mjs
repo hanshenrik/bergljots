496997588f9054ffb4ee499926b895eac50f41af
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-02-27 18:44:52 +0100

    Add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,23 +8,27 @@ export default defineConfig({
   integrations: [
     tailwind(),
     webmanifest({
-      /**
-       * required
-       **/
       name: "Bergljots",
-
-      /**
-       * optional
-       **/
-      icon: "public/favicon.svg",
-
+      description: "På vei ut døra i Bergljots",
+      icons: [
+        {
+          "src": "/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ],
       short_name: "Bergljots",
       description: "På vei ut døra i Bergljots",
       start_url: "/",
       theme_color: "#33B8D6",
       background_color: "#000000",
       display: "minimal-ui",
-      orientation: "landscape"
+      orientation: "landscape",
     }),
   ],
 });
