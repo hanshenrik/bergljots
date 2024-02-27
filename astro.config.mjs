06973d4559bdd113b2e85bd88c20c7bac50238f1
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-02-27 18:25:59 +0100

    Fix webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), webmanifest({
+    /**
+     * required
+     **/
+    name: 'Bergljots',
+
+    /**
+     * optional
+     **/
+    icon: 'public/favicon.svg',
+
+    short_name: 'Bergljots',
+    description: 'På vei ut døra i Bergljots',
+    start_url: '/',
+    theme_color: '#3367D6',
+    background_color: '#3367D6',
+    display: 'standalone',
+  }),]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,8 @@
-import { defineConfig } from 'astro/config';
-import webmanifest from 'astro-webmanifest';
-
-export default defineConfig({
+/** @type {import('tailwindcss').Config} */
+export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-	integrations: [
-		webmanifest({
-		  /**
-		   * required
-		   **/
-		  name: 'Bergljots',
-	
-		  /**
-		   * optional
-		   **/
-		  icon: 'public/favicon.svg', // source for favicon & icons
-	
-		  short_name: 'Bergljots',
-		  description: 'På vei ut døra i Bergljots',
-		  start_url: '/',
-		  theme_color: '#3367D6',
-		  background_color: '#3367D6',
-		  display: 'standalone',
-		}),
-	  ],
-});
+};
