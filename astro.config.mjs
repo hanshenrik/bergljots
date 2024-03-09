df628eb6566c0f8aff9794783b72011794894cb2
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-03-09 10:01:45 +0100

    Fix autoformatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,15 +12,15 @@ export default defineConfig({
       description: "På vei ut døra i Bergljots",
       icons: [
         {
-          "src": "/android-chrome-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png"
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          "src": "/android-chrome-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        }
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
       ],
       short_name: "Bergljots",
       description: "På vei ut døra i Bergljots",
